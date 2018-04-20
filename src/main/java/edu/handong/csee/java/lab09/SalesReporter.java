511fa63ec0c925212f01604b5f22584e50806cbd
baejaeho18 <bih1749@gmail.com>
2018-04-20 18:10:03 +0900

    use arrayList switch branch

--- src/main/java/edu/handong/csee/java/lab09/SalesReporter.java
+++ src/main/java/edu/handong/csee/java/lab09/SalesReporter.java
@@ -1,12 +1,13 @@
 package edu.handong.csee.java.lab09;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class SalesReporter {
 
 	private double highestSales;
 	private double averageSales;
-	private Salesman[] team;		//name:team(salesman;class type) declare
-	private int numberOfSalesman;
+	private Salesman[] list;		//name:arrayList(salesman;class type) declare
+	private int numberOfSalesman=0;
 	private Scanner scanner;		//should be import
 
 	public static void main(String[] args) {
@@ -20,36 +21,39 @@ public class SalesReporter {
 	}
 
 	public void getData() {
-		System.out.println("Enter number of sales associates: ");	//system.out.println(=printf)
-		scanner = new Scanner(System.in);		//(System.in):for scanner
-		numberOfSalesman = scanner.nextInt();	//after instanced by "new", 'instancd name''.''action'
-		scanner.nextLine();
+//		System.out.println("Enter number of sales associates: ");	//system.out.println(=printf)
+//		numberOfSalesman = scanner.nextInt();	//after instanced by "new", 'instancd name''.''action'
+		
+		do {
+			scanner = new Scanner(System.in);		//(System.in):for scanner
 
-		team = new Salesman[numberOfSalesman];	//array; Salesman[number...}
-
-		for(int i=0;i<numberOfSalesman;i++) {
-			System.out.println("Enter data of sales associates: " + (i+1) );//print:"~"+"(~)"
+			System.out.println("Enter data of sales associates" + (numberOfSalesman +1) );//print:"~"+"(~)"
 
 			System.out.println("Enter name of sales associates: ");
 			String name = scanner.nextLine();	//how to get all line
 			System.out.println("Enter associates's sales: $");
 			double sales = scanner.nextDouble();
 			scanner.nextLine();
+			System.out.println("More salesassociates for the list?");
+			String ask = scanner.nextLine(); 	//choice to go or not
+			scanner.nextLine();
+			
+			list[numberOfSalesman] = new Salesman();	//team[i]: Salesman instanced
+			list[numberOfSalesman].setName(name); 		//instance name.public method
+			list[numberOfSalesman].setSales(sales);	//this.sales = sales
+			list[numberOfSalesman].setAsk(ask);		//this.ask = ask
 
-			team[i] = new Salesman();	//team[i]: Salesman instanced
-			//what's different with 23 and 33?
-			team[i].setName(name); 		//instance name.public method
-			team[i].setSales(sales);	//this.sales = sales
-		}				
+			numberOfSalesman = numberOfSalesman + 1;//count how many salesman inputed
+		} while(list[numberOfSalesman].getAsk() == "Yes");		
 	}
 	public void computeStats() {
 		for(int i=0;i<numberOfSalesman;i++) {
 			double highestSales = 0;
-			if(highestSales<team[i].getSales())
-				highestSales = team[i].getSales();
+			if(highestSales<list[i].getSales())
+				highestSales = list[i].getSales();
 			this.highestSales = highestSales;
 
-			averageSales=team[i].getSales()+averageSales;
+			averageSales=list[i].getSales()+averageSales;
 		}
 	}
 
@@ -57,24 +61,24 @@ public class SalesReporter {
 		System.out.println("Average slaes per assoicaite is $" + averageSales);
 
 		for(int i=0;i<numberOfSalesman;i++) {
-			if(team[i].getSales() == highestSales) {
+			if(list[i].getSales() == highestSales) {
 				System.out.println("The highest sales figure is $" + highestSales);
 				System.out.println("The following had the highest sales: ");
-				System.out.println("Name: " + team[i].getName());
-				System.out.println("Sales: $" + team[i].getSales());
-				System.out.println((team[i].getSales()-averageSales) + "above the average.");
+				System.out.println("Name: " + list[i].getName());
+				System.out.println("Sales: $" + list[i].getSales());
+				System.out.println((list[i].getSales()-averageSales) + "above the average.");
 			}
 		}
 
 		for(int i=0;i<numberOfSalesman;i++) {
-			if(team[i].getSales() != highestSales) {
-				System.out.println("Name: " + team[i].getName());
-				System.out.println("Sales: $" + team[i].getSales());
-				if(team[i].getSales()<averageSales) {
-					System.out.println( (averageSales-team[i].getSales()) + "below the average.");
+			if(list[i].getSales() != highestSales) {
+				System.out.println("Name: " + list[i].getName());
+				System.out.println("Sales: $" + list[i].getSales());
+				if(list[i].getSales()<averageSales) {
+					System.out.println( (averageSales-list[i].getSales()) + "below the average.");
 				}
-				if(team[i].getSales()>averageSales)
-					System.out.println( (team[i].getSales()-averageSales) + "above the average.");
+				if(list[i].getSales()>averageSales)
+					System.out.println( (list[i].getSales()-averageSales) + "above the average.");
 			}
 		}
 	}
--- src/main/java/edu/handong/csee/java/lab09/Salesman.java
+++ src/main/java/edu/handong/csee/java/lab09/Salesman.java
@@ -14,8 +14,16 @@ public class Salesman {
 	public void setSales(double sales) {
 		this.sales = sales;
 	}
-//right mouse botton -> source menu -> getters and setters
+	public String getAsk() {
+		return ask;
+	}
+	public void setAsk(String ask) {
+		this.ask = ask;
+	}
+	//right mouse botton -> source menu -> getters and setters
 	private String name;
+	private String ask;
 	private double sales;
+//	private String continue;
 	
 }
